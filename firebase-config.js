cf81c80199462df7581f0c5ca39dbdd15f9bf937
FlylerSky <roesuryvoe@gmail.com>
2025-11-15 10:04:26 +0700

    update

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase
 
 export function initFirebase() {
   const firebaseConfig = {
-  
+  apiKey: "AIzaSyCYxFAVaey6NeM5diL90312tIflIPduBEs",
+  authDomain: "friendsmda-d3e5d.firebaseapp.com",
+  projectId: "friendsmda-d3e5d",
+  storageBucket: "friendsmda-d3e5d.firebasestorage.app",
+  messagingSenderId: "300350999498",
+  appId: "1:300350999498:web:3721aa674682cfa6eadd33",
+  measurementId: "G-J56077Z67G"
 };
 
   const app = initializeApp(firebaseConfig);
